5b7f11df392cf5c1c512fea18f0b35d93198c5b3
Juan David Ospina <juan.ospina@zemoga.com>
2021-04-20 10:24:07 -0500

    Finish lesson 8

--- main.js
+++ main.js
@@ -4,21 +4,39 @@ const app = Vue.createApp({
             cart:0,
             product: 'Socks',
             brand: 'Vue Mastery',
-            image: './assets/images/socks_blue.jpg',
-            inStock: false,
+            selectedVariant: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50, onsale: false},
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0, onsale: false},
             ]
+        }
+    },
+    computed : {
+        title() {
+            return this.brand + this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity > 0 ? true : false
+        }, 
+        onSale() {
+            if(this.variants[this.selectedVariant].onsale) {
+                return this.brand + ' ' + this.product
+            } else {
+                return
+            }
+            
         }
     },
     methods: {
         addToCart() {
             this.cart += 1
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant (index) {
+            this.selectedVariant = index
         }
     }
 })
